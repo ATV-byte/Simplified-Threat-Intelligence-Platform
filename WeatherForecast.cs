e1a392129f4eca0789265c553619864bbec42174
Vlad <adrianvld93@gmail.com>
2025-08-11 22:36:20 +0300

    initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Simplified_Threat_Intelligence_Platform
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
