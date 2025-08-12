69088174d7381861d6ff838dc0d8e1c9f39a4791
Vlad <adrianvld93@gmail.com>
2025-08-12 17:48:39 +0300

    add mongo config

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IMongoDatabase>(sp =>
+{
+    var cfg = builder.Configuration.GetSection("Mongo");
+    var client = new MongoClient(cfg["ConnectionString"]);
+    return client.GetDatabase(cfg["Database"]);
+});
 
 var app = builder.Build();
 
